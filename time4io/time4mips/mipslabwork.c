123e93f133502da25d8bcb9945df9ed701e9f745
Simpan0303 <simonelov03@gmail.com>
2024-02-10 12:15:11 +0100

    u l3 a1

--- time4io/time4mips/mipslabwork.c
+++ time4io/time4mips/mipslabwork.c
@@ -10,6 +10,23 @@
 
    For copyright and licensing, see file COPYING */
 
+
+/*
+PIC32 ChipKit
+In file mipslabwork.c, add code in function labinit to initialize Port E so that bits 7 through 0
+of Port E are set as outputs (i.e., the 8 least significant bits). These bits are connected to 8 green
+LEDs on the Basic IO Shield. Register TRISE has address 0xbf886100.You should initialize the
+port using your own volatile pointer, that is, you should not use the definitions in pic32mx.h, yet.
+Do not change the function (direction) of any other bits of Port E.
+*/
+
+/*
+  In file mipslabwork.c, add code in function labwork to increase the binary value shown on the
+  8 green LEDs once each time the function tick is called. Initialize the value to 0, so that the LEDs
+  show how many "ticks" have occurred since the program was started. See below. You should use
+  your own volatile pointer. Register PORTE has address 0xbf886110
+*/
+
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
@@ -24,10 +41,18 @@ void user_isr( void )
   return;
 }
 
+volatile int *trise;
+volatile int *porte;
 /* Lab-specific initialization goes here */
 void labinit( void )
 {
-  return;
+  TRISD |= 0x0FE0
+
+  volatile int *trise = (volatile int*) 0xbf886100; // Define a volatile pointer to TRIS register of Port E
+  *trise &= 0xFF00; // Set bits 7 through 0 as outputs
+
+  porte = (volatile int*) 0xbf886110; // Define a volatile pointer to PORTE register
+  *porte = 0; // Initialize the value to 0
 }
 
 /* This function is called repetitively from the main program */
@@ -39,4 +64,14 @@ void labwork( void )
   display_update();
   tick( &mytime );
   display_image(96, icon);
+
+
+
+
+
+  (*porte)++; // Increase the binary value shown on the 8 green LEDs
 }
+
+
+
+
